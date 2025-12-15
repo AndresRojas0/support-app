6159b9fd24b466fd911b733cd27ce106faa5d15b
Andres Rojas <andresgustavorojas0@gmail.com>
2025-12-14 22:16:17 -0300

    first commit

---
+++ backend/app/main.py
@@ -0,0 +1,23 @@
+# /backend/app/main.py
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(title="Soporte Técnico API", version="0.1.0")
+
+# Configurar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["http://localhost:3000"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
+@app.get("/")
+def home():
+    return {"message": "Backend funcionando con FastAPI"}
+
+@app.get("/health")
+def health():
+    return {"status": "Todo OK"}
